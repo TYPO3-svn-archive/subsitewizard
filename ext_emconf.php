6027c41def06ca702fadc8ad7ac754cabf587998
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-08 10:34:55 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/subsitewizard/trunk@27452 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "subsitewizard".
+#
+# Auto generated 08-12-2009 11:33
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Subsitewizard',
+	'description' => 'Projekt der TU Ilmenau',
+	'category' => 'module',
+	'author' => 'Steffen Kamper',
+	'author_email' => 'info@sk-typo3.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"1e95";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"eb6c";s:19:"doc/wizard_form.dat";s:4:"9187";s:20:"doc/wizard_form.html";s:4:"051d";s:13:"mod1/conf.php";s:4:"c25c";s:14:"mod1/index.php";s:4:"f1cd";s:18:"mod1/locallang.xml";s:4:"d260";s:22:"mod1/locallang_mod.xml";s:4:"23ec";s:22:"mod1/mod_template.html";s:4:"0db4";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+);
+
+?>
---
+++ mod1/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
+$MCONF['name'] = 'tools_txsubsitewizardM1';
+$MCONF['script'] = '_DISPATCH';
+	
+$MCONF['access'] = 'user,group';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:subsitewizard/mod1/locallang_mod.xml';
+?>
